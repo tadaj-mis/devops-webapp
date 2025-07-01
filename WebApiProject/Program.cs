46a214cefb7c5489b89b635efacace7624aea030
not39021.tada@gmaill.com <160215621+tadaj-mis@users.noreply.github.com>
2025-07-01 15:41:20 +0700

    commit4

--- WebApiProject/Program.cs
+++ WebApiProject/Program.cs
@@ -18,16 +18,14 @@ builder.Services.AddSwaggerGen(c =>
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
 });
 
-var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+(app.Environment.IsDevelopment())
+
     app.UseSwagger();
     app.UseSwaggerUI(c =>
     {
         c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API v1");
     });
-}
 
 using (var scope = app.Services.CreateScope())
 {
